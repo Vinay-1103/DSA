228e96769cb9cff78dee0b8f9ea11c8fd12a5567
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-11-01 21:30:16 +0530

    intial commit

--- src/com/vinay/basics/linkedlist/DoubleLinkedlist/Node.java
+++ src/com/vinay/basics/linkedlist/DoubleLinkedlist/Node.java
@@ -1,5 +1,13 @@
 package com.vinay.basics.linkedlist.DoubleLinkedlist;
 
 public class Node {
-    
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+        this.prev=null;
+    }
 }
