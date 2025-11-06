531acc5db276025c7540f5209e5b654e82fd992e
Vinay kumar Korakoppula <vinaykumar22ra1a6694@gmail.com>
2025-11-06 22:27:25 +0530

    Update ArrayListDemo.java

--- src/com/vinay/basics/arraylist/ArrayListDemo.java
+++ src/com/vinay/basics/arraylist/ArrayListDemo.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class ArrayListDemo {
 //Without Generics
-/*    public static void main(String[] args) {
+   public static void main(String[] args) {
         ArrayList list=new ArrayList();
         list.add(10);
         list.add(15);
@@ -15,7 +15,7 @@ public class ArrayListDemo {
         System.out.println(name);
 
         System.out.println(list);
-    }*/
+    }
 
 //    With Generics
 public static void main(String[] args) {
