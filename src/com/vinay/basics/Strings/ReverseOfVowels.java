c812850ea24bfce482ec5ce367da06e6ad87c0a0
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2026-01-06 23:19:36 +0530

    intial commit

---
+++ src/com/vinay/basics/Strings/ReverseOfVowels.java
@@ -0,0 +1,34 @@
+package com.vinay.basics.Strings;
+
+public class ReverseOfVowels {
+        public static String reverseVowels(String s) {
+            char[] arr = s.toCharArray();
+            int left = 0 ,right = arr.length-1;
+            while(left<right){
+                char leftChar = arr[left];
+                char rightChar = arr[right];
+
+                if("aeiouAEIOU".indexOf(leftChar) == -1){
+                    left++;
+                }
+                else if ("aeiouAEIOU".indexOf(rightChar) == -1){
+                    right--;
+                }
+                else{
+                    arr[left] = rightChar;
+                    arr[right] = leftChar;
+                    left++;
+                    right--;
+                }
+            }
+            return new String(arr);
+
+        }
+
+    public static void main(String[] args) {
+            String s="IceCreAm";
+        System.out.println(reverseVowels(s));
+
+    }
+}
+
