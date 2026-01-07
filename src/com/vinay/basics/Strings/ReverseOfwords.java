b0a861e19b7314b70f1065225cdeda8b1b518aa4
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2026-01-07 22:17:45 +0530

    intial commit

---
+++ src/com/vinay/basics/Strings/ReverseOfwords.java
@@ -0,0 +1,21 @@
+package com.vinay.basics.Strings;
+
+public class ReverseOfwords {
+
+        public static String reverseWords(String s) {
+            String[] words = s.trim().split("\\s+");
+            StringBuilder result = new StringBuilder();
+            for(int i = words.length-1 ; i>=0; i--){
+                result.append(words[i]);
+                if(i!=0){
+                    result.append(" ");
+                }
+            }
+            return result.toString();
+        }
+
+    public static void main(String[] args) {
+        String s=" hello world ";
+        System.out.println(reverseWords(s));
+    }
+}
