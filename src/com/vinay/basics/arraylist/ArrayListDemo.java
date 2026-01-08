0e5b64e07605ecd555926625b1bac7ba1f9b2362
vinayCodesDaily <vkkorakoppula786@gmail.com>
2026-01-08 22:37:12 +0530

    Add Practice folder with DSA programs

--- src/com/vinay/basics/arraylist/ArrayListDemo.java
+++ src/com/vinay/basics/arraylist/ArrayListDemo.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class ArrayListDemo {
 //Without Generics
+    //for checking pull request demo
    public static void main(String[] args) {
         ArrayList list=new ArrayList();
         list.add(10);
