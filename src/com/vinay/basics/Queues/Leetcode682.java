4c17320771f57dff53ecf12403d72891ed7eed6f
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2026-04-16 22:29:06 +0530

    intial commit

---
+++ src/com/vinay/basics/Queues/Leetcode682.java
@@ -0,0 +1,42 @@
+package com.vinay.basics.Queues;
+
+import java.util.Stack;
+
+public class Leetcode682 {
+
+        public static int calPoints(String[] operations) {
+            Stack<Integer> stack = new Stack<>();
+            for(String op : operations){
+                if(op.equals("+")){
+                    int x = stack.pop();
+                    int newScore = x+stack.peek();
+                    stack.push(x);
+                    stack.push(newScore);
+                }
+                else if (op.equals("C")){
+                    stack.pop();
+                }
+                else if(op.equals("D")){
+                    stack.push(2*stack.peek());
+                }
+                else{
+                    stack.push(Integer.parseInt(op));
+                }
+            }
+            int total = 0;
+            for(int score : stack){
+                total += score;
+            }
+            return total;
+        }
+
+    public static void main(String[] args) {
+        String[] operations = {"5","2","C","D","+"};
+
+        int result = calPoints(operations);
+
+        System.out.println(result);
+
+    }
+    }
+
